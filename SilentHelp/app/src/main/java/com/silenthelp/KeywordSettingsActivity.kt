79dd0ce87f6b2167716ac29bffa7411b83582c05
Kelley-rosa <kelley.rosa@bellevuecollege.edu>
2025-05-26 20:53:48 -0700

    Added layout folder and keyword settings

---
+++ SilentHelp/app/src/main/java/com/silenthelp/KeywordSettingsActivity.kt
@@ -0,0 +1,13 @@
+// Created by Kelley Rosa 05-22-25
+
+package com.silenthelp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class KeywordSettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.view_keyword_settings)
+    }
+}
