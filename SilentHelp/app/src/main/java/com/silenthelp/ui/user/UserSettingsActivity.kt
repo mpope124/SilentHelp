f1cb8c420dd2299be54dc27e7a1af628e2fcf096
Colin Jones <colin@texis.com>
2025-06-09 23:28:51 -0700

    Added content for user settings

--- SilentHelp/app/src/main/java/com/silenthelp/ui/user/UserSettingsActivity.kt
+++ SilentHelp/app/src/main/java/com/silenthelp/ui/user/UserSettingsActivity.kt
@@ -1,10 +1,21 @@
 package com.silenthelp.ui.user
 
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.silenthelp.R
 
 class UserSettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.usersettingslayout)
+
+        val backButton: ImageView = findViewById(R.id.btn_back)
+        backButton.setOnClickListener {
+            finish()
+        }
     }
 }
